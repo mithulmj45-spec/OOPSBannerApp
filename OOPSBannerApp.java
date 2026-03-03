5439a4592d85b50572fb25cc18dcfd123bb0182e
Mithul <mithulmj45@gmail.com>
2026-03-03 09:38:31 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,23 @@
+/**
+ * OOPSBannerApp
+ * 
+ * UC1: Print OOPS to Console
+ * 
+ * This application prints the literal text "OOPS"
+ * to the console as the foundational use case.
+ * 
+ * @author Mithul Jegannath
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Printing the literal text OOPS
+        System.out.println("OOPS");
+    }
+}
