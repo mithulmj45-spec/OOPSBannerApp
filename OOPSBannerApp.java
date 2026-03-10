9b435cbe4d736fab97ff39847fae02b8ba299ec1
Mithul <mithulmj45@gmail.com>
2026-03-10 09:15:14 +0530

    UC5: Concise array initialization with String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -59,3 +59,30 @@ public class OOPSBannerApp {
         }
     }
 }
+/**
+ * OOPSBannerApp
+ * UC5: Concise Array Initialization using String.join()
+ *
+ * @author Mithul
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Declare and initialize banner array in a single statement
+        String[] banner = {
+            String.join(" ", "OOOO", "OOOO", "PPPP", "PPPP", "SSSS"),
+            String.join(" ", "O", "O", "P", "P", "S"),
+            String.join(" ", "O", "O", "PPPP", "SSSS"),
+            String.join(" ", "O", "O", "P", "S"),
+            String.join(" ", "OOOO", "P", "SSSS")
+        };
+
+        // Print banner using enhanced for loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
