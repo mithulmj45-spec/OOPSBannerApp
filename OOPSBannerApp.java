ea9148c282666e2f98e522d55f6a1c338a845cce
Mithul <mithulmj45@gmail.com>
2026-03-10 08:56:03 +0530

    UC3: Refactor banner creation using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,34 +1,6 @@
-public class OOPSBannerApp {
-
-    public static void main(String[] args) {
-
-        // Line 1
-        System.out.println("  *****  " + "  *****  " + "  ****** " + "  ****** ");
-
-        // Line 2
-        System.out.println(" *     * " + " *     * " + "  *     *" + " *      *");
-
-        // Line 3
-        System.out.println("*       *" + "*       *" + "  *     *" + "*       ");
-
-        // Line 4
-        System.out.println("*       *" + "*       *" + "  ****** " + " *****   ");
-
-        // Line 5
-        System.out.println("*       *" + "*       *" + "  *      " + "      *  ");
-
-        // Line 6
-        System.out.println(" *     * " + " *     * " + "  *      " + "*     *  ");
-
-        // Line 7
-        System.out.println("  *****  " + "  *****  " + "  *      " + " *****   ");
-    }
-
-
 /**
  * OOPSBannerApp
- * 
- * UC1: Print the literal text "OOPS"
+ * UC3: Print OOPS Banner using String.join()
  * 
  * @author Mithul
  * @version 1.0
@@ -36,13 +8,27 @@ public class OOPSBannerApp {
 
 public class OOPSBannerApp {
 
-    /**
-     * Main method - Entry point of the application
-     */
     public static void main(String[] args) {
 
-        // Print OOPS to console
-        System.out.println("OOPS");
+        String line1 = String.join(" ",
+                "OOOO", "OOOO", "PPPP", "PPPP", "SSSS");
 
+        String line2 = String.join(" ",
+                "O", "O", "P", "P", "S");
+
+        String line3 = String.join(" ",
+                "O", "O", "PPPP", "SSSS");
+
+        String line4 = String.join(" ",
+                "O", "O", "P", "S");
+
+        String line5 = String.join(" ",
+                "OOOO", "P", "SSSS");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
     }
 }
